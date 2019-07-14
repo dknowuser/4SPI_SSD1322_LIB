f6debc6147ed710c04de1e07d6c20f12fa26dda6
dknowuser <maro4uk@yandex.ru>
2019-07-15 01:46:23 +0300

    Sample code

---
+++ sketch_jul08a.ino
@@ -0,0 +1,45 @@
+#include <SPI.h>
+
+#define OLED_DC 12
+#define OLED_CS 11
+#define RES 10
+#define OLED_SCLK 52
+#define OLED_SDIN 51
+
+void setup() {
+  unsigned int i = 0;
+  unsigned int del = 1000;
+  
+  // put your setup code here, to run once:
+  pinMode(OLED_DC, OUTPUT);
+  pinMode(OLED_CS, OUTPUT);
+  pinMode(OLED_SCLK, OUTPUT);
+  pinMode(OLED_SDIN, OUTPUT);
+  pinMode(RES, OUTPUT);
+
+  //digitalWrite(RES, LOW);
+  //delay(del);
+  
+  SPI.setClockDivider(4);
+  SPI.begin();
+  SPI.setBitOrder(MSBFIRST);
+
+  digitalWrite(RES, HIGH);
+  digitalWrite(OLED_DC, LOW);
+  digitalWrite(OLED_CS, LOW);
+  SPI.transfer(0x0A7);
+  digitalWrite(OLED_CS, HIGH);
+
+  while(1) {
+    if(i == 256) i = 0;    
+    digitalWrite(OLED_DC, LOW);
+    digitalWrite(OLED_CS, LOW);
+    SPI.transfer(i++);
+    digitalWrite(OLED_CS, HIGH);
+  };  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
